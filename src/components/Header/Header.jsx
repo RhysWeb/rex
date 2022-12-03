79f7b48b5f74e80b337af1ea57b5d127a82fe7b7
RhysWeb <Rhys25780@hotmail.com>
2022-12-03 20:19:35 +0000

    added a signout button to the account image

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,26 +4,16 @@ import styles from './Header.module.css';
 import { signOut } from 'next-auth/react';
 import { Sansita_Swashed } from '@next/font/google';
 import Logo from '../Logo/Logo';
+import { useState } from 'react';
 
 const sansita = Sansita_Swashed({ subsets: ['latin'] });
 
 const Header = ({ session, flip, toggleFlip }) => {
+	const [showLogout, setShowLogout] = useState(false);
 	return (
 		<div className={styles.hero}>
 			<div className={styles.nameBadge}>
-				<div className={styles.imageContainer}>
-					<Image
-						src={session.user.image}
-						alt="user Image"
-						width={1000}
-						height={1000}
-						placeholder="blur"
-						blurDataURL={session.user.image}
-					/>
-				</div>
-				<div className={styles.flex}>
-					<div className={styles.name}>{session.user.name}</div>
-
+				{showLogout && (
 					<input
 						type="button"
 						value="sign-out"
@@ -32,6 +22,23 @@ const Header = ({ session, flip, toggleFlip }) => {
 						}}
 						className={styles.signOutButton}
 					/>
+				)}
+				<div className={styles.imageContainer}>
+					<Image
+						src={session.user.image}
+						alt="user Image"
+						width={1000}
+						height={1000}
+						placeholder="blur"
+						blurDataURL={session.user.image}
+						onClick={() => {
+							console.log(showLogout);
+							setShowLogout(!showLogout);
+						}}
+					/>
+				</div>
+				<div className={styles.flex}>
+					<div className={styles.name}>{session.user.name}</div>
 				</div>
 			</div>
 			<Logo flip={flip} toggleFlip={toggleFlip} />
